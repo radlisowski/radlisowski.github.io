60578fb781618b7984857076e52857454c25ed18
Rad Lisowski <radlisowski@gmail.com>
2020-10-27 21:37:34 +0000

    changed teh protocol to https

--- weather/app.js
+++ weather/app.js
@@ -43,7 +43,7 @@ function showError(error) {
 
 //get weather from the api
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     
     fetch(api)
     .then(function(response){
