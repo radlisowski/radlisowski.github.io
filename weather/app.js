6b14bfc0a30635bc40ad57184adc9e7320180539
Rad Lisowski <radlisowski@gmail.com>
2021-01-05 06:07:52 +0000

    .

--- weather/app.js
+++ weather/app.js
@@ -94,7 +94,7 @@ function updateWallpapper() {
         favIcon.href = "/weather/img/sunny.png"
     }
 
-    if (/rain/.test(weather.description)) {
+    if (/snow/.test(weather.description)) {
         document.body.style.background = "#293251 url('https://images.unsplash.com/photo-1581413084636-d581a1d9a268?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2091&q=80') no-repeat";
         favIcon.href = "/weather/img/rain.png"
     }
