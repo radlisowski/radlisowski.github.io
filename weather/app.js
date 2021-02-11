1f05f4a4a16d4eddc209d34f7bb5ef60f8ca7278
Rad Lisowski <radlisowski@gmail.com>
2021-02-11 08:31:14 +0000

    fiuxed isssue with walpaper condition changed = 0 to < 1

--- weather/app.js
+++ weather/app.js
@@ -100,7 +100,7 @@ function updateWallpapper() {
         favIcon.href = "/weather/img/rain.png"
     }
 
-    if (weather.temperature.value < 1 || weather.feelsLike.value == 1) {
+    if (weather.temperature.value < 1 || weather.feelsLike.value < 1) {
         document.body.style.background = "#293251 url('https://images.unsplash.com/photo-1496340077100-9573d8b77463?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1962&q=80') no-repeat";
         favIcon.href = "/weather/img/rain.png"
     }
