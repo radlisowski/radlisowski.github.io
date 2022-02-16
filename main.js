7e14fa38329546f57234ae1737b7114e6212984b
Rad Lisowski <radlisowski@gmail.com>
2022-02-16 22:40:43 +0000

    added funconating login page

---
+++ main.js
@@ -0,0 +1,29 @@
+
+const loginForm = document.getElementById("login-form");
+const successMessage = document.getElementById("success-message");
+const errorMessage = document.getElementById("error-message");
+const loginButton = document.getElementById("signin-button");
+const username = "testUsername";
+const password = "testPassword";
+
+loginButton.addEventListener("click", function () {
+    let enteredUsername = document.getElementById("username-field").value;
+    let enterPassword = document.getElementById("password-field").value;
+
+
+    if (enterPassword == password && enteredUsername == username) {
+        successMessage.style.display = "block";
+        loginForm.style.display = "none";
+    } else {
+        errorMessage.style.display = "block";
+        loginForm.style.display = "none";
+    }
+});
+
+errorMessage.addEventListener("click", function () {
+    location.reload();
+});
+
+successMessage.addEventListener("click", function () {
+    location.reload();
+});
