6676fe0933252965d8b562d596963b82fb93cdf9
Rad Lisowski <radlisowski@gmail.com>
2022-02-16 22:52:40 +0000

    odd glitch on chrombook display where the buttons were displayed even when parent div was set to diplay none

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
 const loginForm = document.getElementById("login-form");
+
 const successMessage = document.getElementById("success-message");
 const errorMessage = document.getElementById("error-message");
+const successButton = document.getElementById("success-button");
+const errorButton = document.getElementById("error-button");
+
 const loginButton = document.getElementById("signin-button");
 const username = "testUsername";
 const password = "testPassword";
@@ -13,9 +17,11 @@ loginButton.addEventListener("click", function () {
 
     if (enterPassword == password && enteredUsername == username) {
         successMessage.style.display = "block";
+        successButton.style.display = "block";
         loginForm.style.display = "none";
     } else {
         errorMessage.style.display = "block";
+        errorButton.style.display = "block";
         loginForm.style.display = "none";
     }
 });
