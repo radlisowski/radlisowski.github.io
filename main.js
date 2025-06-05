678936961fea22e817078f4c7fb7e79b312514aa
Radoslaw Lisowski <radlisowski@gmail.com>
2025-06-05 07:05:14 +0100

    the updated login page and login goes to the index page now

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 
 const loginForm = document.getElementById("login-form");
-
-const successMessage = document.getElementById("success-message");
 const errorMessage = document.getElementById("error-message");
-const successButton = document.getElementById("success-button");
 const errorButton = document.getElementById("error-button");
-
 const loginButton = document.getElementById("signin-button");
 const username = "test";
 const password = "test";
@@ -15,15 +11,20 @@ window.onload = function() {
     document.getElementById("username-field").focus();
   };
 
+loginButton.addEventListener("mouseover", function (){
+    loginButton.style.transform = "scale(1.07)";
+})
+
+loginButton.addEventListener("mouseout", function () {
+    loginButton.style.transform = "scale(1)";
+})
+
 loginButton.addEventListener("click", function () {
     let enteredUsername = document.getElementById("username-field").value;
     let enterPassword = document.getElementById("password-field").value;
 
-
-    if (enterPassword == password && enteredUsername == username) {
-        successMessage.style.display = "block";
-        successButton.style.display = "block";
-        loginForm.style.display = "none";
+    if (enterPassword === password && enteredUsername === username) {
+        window.location.href = "index.html";
     } else {
         errorMessage.style.display = "block";
         errorButton.style.display = "block";
@@ -34,15 +35,11 @@ loginButton.addEventListener("click", function () {
 document.getElementById("password-field")
     .addEventListener("keyup", function(event) {
     event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("signin-button").click();
+        if (event.key === "Enter") {
+            document.getElementById("signin-button").click();
     }
 });
 
 errorMessage.addEventListener("click", function () {
-    location.reload();
-});
-
-successMessage.addEventListener("click", function () {
     location.reload();
 });
