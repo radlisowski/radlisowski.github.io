23710bf0aeb9dc0a10df205b350b4c1c7632f0b7
radlisowski <radlisowski@gmail.com>
2025-07-10 20:49:11 +0100

    concted api

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ const loginForm = document.getElementById("login-form");
 const errorMessage = document.getElementById("error-message");
 const errorButton = document.getElementById("error-button");
 const loginButton = document.getElementById("signin-button");
-const username = "test";
-const password = "test";
-
 
 window.onload = function() {
     document.getElementById("username-field").focus();
@@ -19,17 +16,38 @@ loginButton.addEventListener("mouseout", function () {
     loginButton.style.transform = "scale(1)";
 })
 
-loginButton.addEventListener("click", function () {
+loginButton.addEventListener("click", async function () {
     let enteredUsername = document.getElementById("username-field").value;
     let enterPassword = document.getElementById("password-field").value;
 
-    if (enterPassword === password && enteredUsername === username) {
+    const apiURL = "https://api-project-yrxg.onrender.com/users/check";
+try {
+      const response = await fetch(apiURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "username": enteredUsername,
+          "password": enterPassword
+        })
+      });
+
+      if (response.ok) {
+        const data = response.json();
+        errorMessage.style.display = "none";
         window.location.href = "index.html";
-    } else {
+        // Optionally: redirect or store token here
+      } else {
         errorMessage.style.display = "block";
-        errorButton.style.display = "block";
-        loginForm.style.display = "none";
+          errorButton.style.display = "block";
+          loginForm.style.display = "none";
+      }
+    } catch (err) {
+      console.error("Login error:", err);
+      errorMessage.style.display = "block";
     }
+    
 });
 
 document.getElementById("password-field")
